04d8e2f8d001a459bf3731c6d83870d6d32a8968
aeryli <72891343+aeryli@users.noreply.github.com>
2026-04-23 20:42:40 -0700

    Update keyboard.js

--- online/mobile/keyboard.js
+++ online/mobile/keyboard.js
@@ -1,10 +1,37 @@
+class keys {
+  constructor() {
+    this.arrows = {
+      "left": {
+        keyCode: 37,
+        keyName: "ArrowLeft",
+        keyNameAsString: "ArrowLeft",
+      },
+      "up": {
+        keyCode: 38,
+        keyName: "ArrowUp",
+        keyNameAsString: "ArrowUp",
+      },
+      "right": {
+        keyCode: 39,
+        keyName: "ArrowRight",
+        keyNameAsString: "ArrowRight",
+      },
+      "down": {
+        keyCode: 40,
+        keyName: "ArrowDown",
+        keyNameAsString: "ArrowDown",
+      }
+    };
+  };
+}
 class kboard {
-  press(key, code, keycode) {
+  press(key) {
     let body = document.body;
+    let keys = new keys();
     let keyEvent = new KeyboardEvent('keydown', {
-        key: key,
-        code: code,
-        keyCode: keycode,
+        key: keys.arrows[key].keyName,
+        code: keys.arrows[key].keyName,
+        keyCode: keys.arrows[key].keyCode,
         bubbles: true,
         cancelable: true
     });
