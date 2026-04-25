886fc4e3f661625fdbb4f23ca609da5ed2f9c3b2
aeryli <72891343+aeryli@users.noreply.github.com>
2026-04-25 11:51:02 -0700

    Create keyboard.js

---
+++ online/kindle/keyboard.js
@@ -0,0 +1,43 @@
+class keys {
+  constructor() {
+    this.arrows = {
+      "left": {
+        keyCode: 37,
+        keyName: "ArrowLeft",
+        keyNameAsString: "ArrowLeft",
+      },
+      "up": {
+        keyCode: 38,
+        keyName: "ArrowUp",
+        keyNameAsString: "ArrowUp",
+      },
+      "right": {
+        keyCode: 39,
+        keyName: "ArrowRight",
+        keyNameAsString: "ArrowRight",
+      },
+      "down": {
+        keyCode: 40,
+        keyName: "ArrowDown",
+        keyNameAsString: "ArrowDown",
+      }
+    };
+  };
+}
+const kys = new keys();
+class kboard {
+  press(key) {
+    let body = document.body;
+    //let keys = new keys();
+    let keyEvent = new KeyboardEvent('keydown', {
+        key: kys.arrows[key].keyName,
+        code: kys.arrows[key].keyName,
+        keyCode: kys.arrows[key].keyCode,
+        bubbles: true,
+        cancelable: true
+    });
+    
+    body.dispatchEvent(keyEvent);
+
+  };
+};
